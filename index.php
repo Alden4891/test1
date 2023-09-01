e7ee8e9cccc008571f48596e5619378696f5d29b
Alden A. Quiñones <39073541+Alden4891@users.noreply.github.com>
2023-09-01 10:23:28 +0800

    initial upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/sys_tracker/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
